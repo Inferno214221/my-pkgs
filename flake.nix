2e37a34b4a6c402a753f5e33ef3982e581f6d230
inferno214221 <inferno214221@gmail.com>
2026-01-28 22:26:32 +1030

    Adds Time Tracker package.

--- flake.nix
+++ flake.nix
@@ -18,6 +18,11 @@
       url = "github:inferno214221/kali-dark-vscode";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    time-tracker = {
+      url = "github:inferno214221/time-tracker";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
@@ -26,12 +31,14 @@
     flake-utils,
     timekeeper,
     simple-tab-groups,
-    kali-dark-vscode
+    kali-dark-vscode,
+    time-tracker
   }: flake-utils.lib.eachDefaultSystem (system: {
     packages = {
       timekeeper = timekeeper.packages."${system}".default;
       simple-tab-groups = simple-tab-groups.packages."${system}".default;
       kali-dark-vscode = kali-dark-vscode.packages."${system}".default;
+      time-tracker = time-tracker.packages."${system}".default;
     };
   });
 }
